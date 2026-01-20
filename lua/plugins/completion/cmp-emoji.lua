36f908b38e06798ba15361592c638f1aabff0e2c
N1xev <alasamouly@gmail.com>
2026-01-21 01:08:29 +0200

    feat(ui): integrate emoji picker and changed its keybind
    - Integrated Snacks icon picker for searching emojis and nerd fonts
    
    - Supports emoji, gitmoji, nerd fonts, and kaomoji sources
    
    - Mapped to <leader>se for quick access
    
    - Replaces previous emoji.nvim implementation
    
    - also you can use cmp-emoji for example in markdown file: `:heart:` and
    it will be heart emoji

--- lua/plugins/completion/cmp-emoji.lua
+++ lua/plugins/completion/cmp-emoji.lua
@@ -1,9 +1,17 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
+  },
+  {
+    "folke/snacks.nvim",
+    keys = {
+      {
+        "<leader>se",
+        function()
+          Snacks.picker.icons()
+        end,
+        desc = "Search Icons and Emojis",
+      },
+    },
   },
 }
